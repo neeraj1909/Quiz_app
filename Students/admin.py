529e2faadc1c1061983beb5120f27377ebb80962
neeraj1909 <neeraj1909@gmail.com>
2017-11-15 01:49:34 +0530

    commit

--- Students/admin.py
+++ Students/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Question, Student, Choice
+
+admin.site.register(Question)
+admin.site.register(Student)
+admin.site.register(Choice)
--- Students/models.py
+++ Students/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 # Create your models here.
+
 class Student(models.Model):
     YEAR_CHOICES = (
         ('1', 'FIRST YEAR'),
@@ -22,7 +23,30 @@ class Student(models.Model):
     roll_no = models.IntegerField(primary_key= True)
     first_name = models.CharField(max_length= 20)
     last_name = models.CharField(max_length= 20)
-    email_id = models.EmailField(max_length= 30)
+    email_id = models.EmailField(max_length= 30, blank= True)
     mobile_number = models.IntegerField(unique= True)
     year = models.CharField(max_length=1, choices= YEAR_CHOICES)
     hostel_name = models.CharField(max_length= 1, choices= HOSTEL_NAME)
+
+    def __str__(self):
+        return ("%s %s" % (self.first_name, self.last_name))
+
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return  self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    #add a boolean attribute to check if it is a correct-choice
+    is_correct_choice = models.BooleanField()
+
+    def __str__(self):
+        return self.choice_text
+
