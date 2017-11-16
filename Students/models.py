d273630cbbd8118c801f67a81dc263b55f9ead51
neeraj1909 <neeraj1909@gmail.com>
2017-11-17 02:13:24 +0530

    third commit

--- Students/models.py
+++ Students/models.py
@@ -45,7 +45,7 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     #add a boolean attribute to check if it is a correct-choice
-    is_correct_choice = models.BooleanField()
+    is_correct_choice = models.BooleanField(default= True)
 
     def __str__(self):
         return self.choice_text
--- Students/views.py
+++ Students/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response, get_object_or_404
+from django.http import request, HttpResponse
+from django.template import RequestContext
+#from django.urls import reverse
+
+from Students.models import Student, Question, Choice
 
 # Create your views here.
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'Students/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the result of question %s."
+    return HttpResponse(response % question_id)
+
+def index(request):
+    context = RequestContext(request)
+    return render_to_response( "quiz/index.html", {}, context_instance = context)
+
+
