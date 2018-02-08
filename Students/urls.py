24bc8e22d73b86eb092a820df05fbf8726c89c74
akashgiricse <akashgiricse@gmail.com>
2018-02-09 00:25:59 +0530

    Inherited base to every file

--- Students/urls.py
+++ Students/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url
 
-from . import views
+from Students import views
 
+app_name = 'Students'
 
 urlpatterns = [
     url(r'^index$', views.index, name='index'),
+    url(r'^register$', views.register, name='register'),
+    url(r'^login$', views.login, name='login'),
+
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^(?P<question_id>[0-9]+)/results/$',views.results, name='results'),
 
